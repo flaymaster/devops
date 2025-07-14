024e079c27f7e06d9827fe5eda6629ef471d0a6f
Tomer Shoob <tomer.shoob@MAC-TOMERSH.local>
2025-07-14 10:11:07 +0300

    fix docker

--- docker/rest-docker/src/rest_docker/app.py
+++ docker/rest-docker/src/rest_docker/app.py
@@ -3,7 +3,7 @@ import boto3
 import os
 import time
 import json
-
+from aws_lambda_powertools import Logger
 app = Flask(__name__)
 
 # AWS clients
@@ -14,7 +14,7 @@ sqs = boto3.client('sqs', region_name=os.environ.get(
 
 
 def get_expected_token():
-    param_name = os.environ.get('TOKEN_PARAM_NAME')
+    param_name = os.environ.get('TOKEN_PARAM_NAME', '/elb/token')
     response = ssm.get_parameter(Name=param_name, WithDecryption=True)
     param_value = response['Parameter']['Value']
     # Parse the JSON string and extract the token
@@ -23,6 +23,7 @@ def get_expected_token():
 
 @app.route("/", methods=["POST"])
 def handle_event():
+    logger = Logger(service="rest-docker")
     try:
         payload = request.get_json()
         if not payload or "data" not in payload or "token" not in payload:
@@ -32,26 +33,25 @@ def handle_event():
         data = payload["data"]
 
         EXPECTED_TOKEN = get_expected_token()
-        print(f"Expected token: {EXPECTED_TOKEN}")
+        logger.info(f"Expected token: {EXPECTED_TOKEN}")
         # Token validation
         if token != EXPECTED_TOKEN:
             return jsonify({"error": "Invalid token"}), 403
-        print("Token validated successfully")
-
+        logger.info("Token validated successfully")
         # ensure email timestream is present and valid
         ts = data.get("email_timestream")
         try:
             time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(int(ts)))
-            print(f"Email timestream is valid: {ts}")
+            logger.info(f"Email timestream is valid: {ts}")
         except Exception:
             return jsonify({"error": "Invalid email_timestream"}), 400
 
         # Send to SQS
         queue_url = os.environ["SQS_QUEUE_URL"]
         sqs.send_message(QueueUrl=queue_url,
-                         MessageBody=payload['data'])
-        print(f"SQS event sent to {queue_url} with data: {payload['data']}")
-
+                         MessageBody=json.dumps(payload['data']))
+        logger.info(
+            f"SQS event sent to {queue_url} with data: {payload['data']}")
         return jsonify({"message": "Accepted"}), 200
 
     except Exception as e:
@@ -64,4 +64,5 @@ def health_check():
 
 
 if __name__ == "__main__":
+    handle_event()
     app.run(host='0.0.0.0', port=80)
